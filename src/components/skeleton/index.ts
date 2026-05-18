43871784efe892dbbfbd7ba796d00d43abb3d21d
Dan Mueller <dpmueller20@gmail.com>
2026-05-18 10:29:47 -0400

    feat(ui): add skeleton kit barrel export

---
+++ src/components/skeleton/index.ts
@@ -0,0 +1,8 @@
+export { Skeleton, SkeletonText, LoadingLabel } from "./skeleton";
+export {
+  SkeletonCard,
+  SkeletonKpi,
+  SkeletonTable,
+  SkeletonChart,
+  SkeletonForm,
+} from "./skeleton-blocks";
